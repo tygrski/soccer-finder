b91419e47698e92c809b4bccf64b172580d9eb78
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-23 10:14:19 -0500

    map

--- assets/script.js
+++ assets/script.js
@@ -6,32 +6,41 @@ var locations = [
   ["Sanchez Soccer Field", 30.2571, -97.7354],
   ["Quarry Field", 30.3992, -97.7358],
 ];
+var fieldList = document.getElementById("selected-fields")
+const infowindow = new google.maps.InfoWindow();
 
-// map display and markers
-
+// map display and center to selected city(Austin)
 var map = new google.maps.Map(document.getElementById("map"), {
   zoom: 10,
   center: new google.maps.LatLng(30.266666, -97.73333),
 });
-const infowindow = new google.maps.InfoWindow();
 
+  // lop through locations array
 for (var i = 0; i < locations.length; i++) {
-  console.log(locations[i][1], locations[i][2]);
+  // console.log(locations[i][1], locations[i][2]);
+
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(locations[i][1], locations[i][2]),
     map: map,
   });
   
+  // function to display locations on marker and add to a list
   marker.addListener("click",(function(marker, i)  {
   
-  return  function() {
+    return  function() {
+    
+    listItem = document.createElement('li');
+
+    // Add the item text
+    listItem.innerHTML = locations[i][0];
+  
+    // Add listItem to the listElement
+    fieldList.appendChild(listItem)
+    
+    // display location name on marker
     infowindow.setContent(locations[i][0])
     infowindow.open(map, marker)
   }
-})(marker,i));
+})
+(marker,i));
 }
-
-// addMarker({
-//   coords:{lat: 30.1980, lng: -97.8836},
-//   // content:'<h1>Slaughter Creek Fields 4</h1>'
-// });
